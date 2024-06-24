b2f6dd52ff031e5952bb36d7212be35ad1c211c3
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-06-24 13:59:03 +0300

    add

--- src/generics/5.ts
+++ src/generics/5.ts
@@ -1,15 +1,17 @@
 enum UserRole {
-    admin = "admin",
-    editor = "editor",
-    guest = "guest",
-  }
-  type margeRole = Record<UserRole, string>;
-  
-  const RoleDescription: Record<UserRole, string> = {
-    [UserRole.admin]: "Admin User",
-    [UserRole.editor]: "Editor User",
-    [UserRole.guest]: "Guest User",
-  };
-  console.log(RoleDescription[UserRole.admin]);
-  console.log(RoleDescription[UserRole.editor]);
-  console.log(RoleDescription[UserRole.guest]);
+  admin = "admin",
+  editor = "editor",
+  guest = "guest",
+}
+
+type margeRole = Record<UserRole, string>;
+
+const RoleDescription: margeRole = {
+  [UserRole.admin]: "Admin User",
+  [UserRole.editor]: "Editor User",
+  [UserRole.guest]: "Guest User",
+};
+
+console.log(RoleDescription[UserRole.admin]);
+console.log(RoleDescription[UserRole.editor]); 
+console.log(RoleDescription[UserRole.guest]); 
