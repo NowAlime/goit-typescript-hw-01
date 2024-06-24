5b9f1a226ec8c550d91c89afda132504d94e5598
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-06-24 13:31:51 +0300

    add

---
+++ goit-typescropt-hw-01/src/basic/1.ts
@@ -0,0 +1,13 @@
+type UserOne = {
+    age: number;
+    username: string;
+    toggle: boolean;
+    empty: null;
+  };
+  
+  const callback = (a: number): number => {
+    return 100 + a;
+  };
+  
+  console.log(callback(6)); 
+  
